fd76e43d4d68930194903422727eccddbc6af64e
Jaslins <jaslins88@gmail.com>
2025-07-18 12:28:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,5 @@ provider "aws"{
 region = "us-east-1"
 }
 resource "aws_ism_user" "tab" {
-name = var.oops
-}
-variable "oops" {
-    description = "enter the value"
-    type = string
-    default = "thara" 
-  
+name = "poiyreqw"
 }
